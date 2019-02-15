e3b169a558d15298ad6b41cf6685e29369b4b9d1
llxqb <llxqb_tt@126.com>
2019-02-15 11:43:04 +0800

    增加启动页淡出动画

--- app/src/main/java/com/liliu/app/mytestapp/activity/SplashActivity.java
+++ app/src/main/java/com/liliu/app/mytestapp/activity/SplashActivity.java
@@ -1,16 +1,35 @@
 package com.liliu.app.mytestapp.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.liliu.app.mytestapp.BaseActivity;
+import com.liliu.app.mytestapp.MainActivity;
 import com.liliu.app.mytestapp.R;
 
 public class SplashActivity extends BaseActivity {
-
+    private Handler handler;
+    private ImageView ivSplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        ivSplash = findViewById(R.id.iv_splash);
+
+        //设置淡入
+        ivSplash.setAlpha(0f);
+        ivSplash.animate().alpha(1f).setDuration(2500);
+
+        Runnable runnable = () -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+        };
+
+        handler.postDelayed(runnable, 2500);
     }
 }
